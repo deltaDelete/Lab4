3d1b1e1699a4e63dc64e65f406402da101fb8cc1
deltaDelete <samansy@yandex.ru>
2023-10-05 13:13:20 +0700

    fix: настройки не получались

--- app/src/main/java/ru/deltadelete/lab5/helpers/SharedPreferencesHelper.java
+++ app/src/main/java/ru/deltadelete/lab5/helpers/SharedPreferencesHelper.java
@@ -6,73 +6,91 @@ import android.content.SharedPreferences;
 public class SharedPreferencesHelper {
     public static final String PREFERENCES_NAME = "Lab7Prefs";
 
+    private static SharedPreferences prefs;
+
     public static void putString(Context context, String key, String value) {
-        SharedPreferences prefs = context.getSharedPreferences(
-                PREFERENCES_NAME,
-                Context.MODE_PRIVATE
-        );
+        if (prefs == null) {
+            prefs = context.getSharedPreferences(
+                    PREFERENCES_NAME,
+                    Context.MODE_PRIVATE
+            );
+        }
         SharedPreferences.Editor editor = prefs.edit();
         editor.putString(key, value);
         editor.apply();
     }
 
     public static void putBool(Context context, String key, boolean value) {
-        SharedPreferences prefs = context.getSharedPreferences(
-                PREFERENCES_NAME,
-                Context.MODE_PRIVATE
-        );
+        if (prefs == null) {
+            prefs = context.getSharedPreferences(
+                    PREFERENCES_NAME,
+                    Context.MODE_PRIVATE
+            );
+        }
         SharedPreferences.Editor editor = prefs.edit();
         editor.putBoolean(key, value);
         editor.apply();
     }
     public static void putInt(Context context, String key, int value) {
-        SharedPreferences prefs = context.getSharedPreferences(
-                PREFERENCES_NAME,
-                Context.MODE_PRIVATE
-        );
+        if (prefs == null) {
+            prefs = context.getSharedPreferences(
+                    PREFERENCES_NAME,
+                    Context.MODE_PRIVATE
+            );
+        }
         SharedPreferences.Editor editor = prefs.edit();
         editor.putInt(key, value);
         editor.apply();
     }
     public static void putFloat(Context context, String key, float value) {
-        SharedPreferences prefs = context.getSharedPreferences(
-                PREFERENCES_NAME,
-                Context.MODE_PRIVATE
-        );
+        if (prefs == null) {
+            prefs = context.getSharedPreferences(
+                    PREFERENCES_NAME,
+                    Context.MODE_PRIVATE
+            );
+        }
         SharedPreferences.Editor editor = prefs.edit();
         editor.putFloat(key, value);
         editor.apply();
     }
 
     public static String getString(Context context, String key) {
-        SharedPreferences prefs = context.getSharedPreferences(
-                PREFERENCES_NAME,
-                Context.MODE_PRIVATE
-        );
-        return prefs.getString("key", "");
+        if (prefs == null) {
+            prefs = context.getSharedPreferences(
+                    PREFERENCES_NAME,
+                    Context.MODE_PRIVATE
+            );
+        }
+        return prefs.getString(key, "");
     }
 
     public static int getInt(Context context, String key) {
-        SharedPreferences prefs = context.getSharedPreferences(
-                PREFERENCES_NAME,
-                Context.MODE_PRIVATE
-        );
-        return prefs.getInt("key", 0);
+        if (prefs == null) {
+            prefs = context.getSharedPreferences(
+                    PREFERENCES_NAME,
+                    Context.MODE_PRIVATE
+            );
+        }
+        return prefs.getInt(key, Context.MODE_PRIVATE);
     }
 
     public static float getFloat(Context context, String key) {
-        SharedPreferences prefs = context.getSharedPreferences(
-                PREFERENCES_NAME,
-                Context.MODE_PRIVATE
-        );
-        return prefs.getFloat("key", 0f);
+        if (prefs == null) {
+            prefs = context.getSharedPreferences(
+                    PREFERENCES_NAME,
+                    Context.MODE_PRIVATE
+            );
+        }
+        return prefs.getFloat(key, 0f);
     }
 
     public static boolean getBool(Context context, String key) {
-        SharedPreferences prefs = context.getSharedPreferences(
-                PREFERENCES_NAME,
-                Context.MODE_PRIVATE
-        );
-        return prefs.getBoolean("key", false);
+        if (prefs == null) {
+            prefs = context.getSharedPreferences(
+                    PREFERENCES_NAME,
+                    Context.MODE_PRIVATE
+            );
+        }
+        return prefs.getBoolean(key, false);
     }
 }
